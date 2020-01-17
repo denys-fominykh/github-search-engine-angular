b2cbc31d77a1928b50d7f4c3c9fe1096c8b17112
denis-fominykh <fominykhdenis@gmail.com>
2020-01-17 23:55:16 +0200

    adding github client id and client secret

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -4,9 +4,12 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class UserService {
+  private clientId = '1b9a0ab83be88a82a5b3';
+  private clientSecret = 'd12903ccc4bb851b0d67ca0ead54696eeeddfb86';
+
   constructor(private http: HttpClient) { }
 
   getUser(username: string): Observable<any> {
-    return this.http.get(`https://api.github.com/users/${username}`);
+    return this.http.get(`https://api.github.com/users/${username}?client_id=${this.clientId}&client_secret=${this.clientSecret}`);
   }
 }
