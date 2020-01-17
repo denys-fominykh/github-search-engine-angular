348277846a04a1c16f402f34f019480ea4117efd
denis-fominykh <fominykhdenis@gmail.com>
2020-01-17 19:19:19 +0200

    creating the toolbar and search components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
